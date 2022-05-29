81e310c22eba92e7e2c852e2042df5bd00f54fde
harryhole13 <harryhole13@gmail.com>
2022-05-29 23:23:12 +0300

    first coommit

---
+++ Navigation/ProfileView.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by Алексей Потемин on 28.05.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+   
+    @IBOutlet weak var avatar: UIImageView!
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    
+    @IBOutlet weak var birthday: UILabel!
+    
+    @IBOutlet weak var userCity: UILabel!
+    
+    @IBOutlet weak var userText: UITextView!
+    
+    
+}
---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Алексей Потемин on 29.05.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let screenWidth = UIScreen.main.bounds.width
+        //let screenHeight = UIScreen.main.bounds.height
+
+        if let myView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            myView.frame = CGRect(x: 16, y: 40, width: screenWidth-32, height: 400)
+            //myView.frame = self.view
+            //myView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            view.addSubview(myView)
+            
+            
+        }
+    }
+    
+
+     
+
+}
