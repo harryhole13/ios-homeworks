645a7b1ec286cb571d65c099206d936276c4b2b2
harryhole13 <harryhole13@gmail.com>
2022-06-01 01:10:49 +0300

    add constraints

--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -16,7 +16,7 @@ class ProfileViewController: UIViewController {
         //let screenHeight = UIScreen.main.bounds.height
 
         if let myView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
-            myView.frame = CGRect(x: 16, y: 40, width: screenWidth-32, height: 400)
+            myView.frame = CGRect(x: 16, y: 40, width: screenWidth-32, height: 350)
             //myView.frame = self.view
             //myView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
             view.addSubview(myView)
